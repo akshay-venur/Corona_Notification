68101c003f66afce746b5983eb68daf66d28ea59
akshay-1612 <akshayk.cs17@sahyadri.edu.in>
2020-09-17 23:44:04 +0530

    added program file

---
+++ main.py
@@ -0,0 +1,54 @@
+from plyer import notification
+import requests
+import bs4
+
+def stat():
+    Total_CoronaVirus_cases = data[0]
+    Deaths = data[1]
+    Recovered = data[2]
+    
+    return "Total cases : "+Total_CoronaVirus_cases+"\n" +"Total Deaths : "+Deaths +"\n"+"Total Recovered : "+Recovered
+
+
+
+def notifyme(title,message):
+    notification.notify(
+        title = title,
+        message = message,
+        app_icon = "icon.ico" ,
+        timeout = 10
+    )
+
+
+if __name__ == "__main__":
+    
+    #make requests from web page
+    result = requests.get('https://www.worldometers.info/coronavirus/country/india')
+
+    #soup object
+    soup = bs4.BeautifulSoup(result.text,'lxml')
+
+    #find the div
+    cases = soup.find_all('div',class_='maincounter-number')
+
+    #data list
+    data=[]
+    for i in cases:
+        span = i.find('span')
+        data.append(span.string)
+
+    notifyme("CoronaVirus Update",stat())
+
+
+
+
+    
+
+   
+
+    
+
+        
+
+    
+    
